7013bbd3fede508ff2fc5016b10f01befd27e17c
oublie6 <47771412+oublie6@users.noreply.github.com>
2026-03-12 13:21:44 +0800

    enforce power-of-two sender concurrency for shard picking

--- src/sender/shard_index.go
+++ src/sender/shard_index.go
@@ -6,24 +6,5 @@ func nextShardIndex(next *atomic.Uint64, concurrency int) int {
 	if concurrency <= 1 {
 		return 0
 	}
-	if concurrency&(concurrency-1) == 0 {
-		return int(next.Add(1)-1) & (concurrency - 1)
-	}
-	bound := uint64(concurrency)
-	for {
-		cur := next.Load()
-		if cur >= bound {
-			if next.CompareAndSwap(cur, 0) {
-				return 0
-			}
-			continue
-		}
-		nxt := cur + 1
-		if nxt == bound {
-			nxt = 0
-		}
-		if next.CompareAndSwap(cur, nxt) {
-			return int(cur)
-		}
-	}
+	return int(next.Add(1)-1) & (concurrency - 1)
 }
